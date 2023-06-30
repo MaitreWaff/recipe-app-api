b9b81493f9d8fdcf375f267a6086e1e3cf7256d9
MaitreWaff <waffoluc@gmail.com>
2023-06-30 01:34:14 +0100

    Preparing for publishing on Production environment.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3in%n!l3dvs$=&yra@mvff%=hun6rka3*j+t05yp!o)_ugwegt'
+# SECRET_KEY = 'django-insecure-3in%n!l3dvs$=&yra@mvff%=hun6rka3*j+t05yp!o)_ugwegt'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'changeme')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
